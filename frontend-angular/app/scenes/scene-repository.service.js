0e59563a60a92ead70d530a1780c1c3167c476be
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-08-30 13:44:12 +0200

    Separate the list of scenes by type in the control view.

--- frontend-angular/app/scenes/scene-repository.service.js
+++ frontend-angular/app/scenes/scene-repository.service.js
@@ -3,11 +3,11 @@
 // This services provides a repository of scene definitions.
 angular.module('scenes').factory('sceneRepositoryService', function() {
   var scenes = [
-    {"id": "0", "title": "live piano stream"},
-    {"id": "1", "title": "please stand by"},
-    {"id": "2", "title": "youtube jingle"},
-    {"id": "3", "title": "twitch jingle"},
-    {"id": "4", "title": "facebook jingle"}
+    {id: "0", title: "live piano stream", type: "live"},
+    {id: "1", title: "please stand by"},
+    {id: "2", title: "youtube jingle"},
+    {id: "3", title: "twitch jingle", type: "jingle"},
+    {id: "4", title: "facebook jingle"}
   ];
   return {
     "scenes": scenes
