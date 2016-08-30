e64932059d33e08aeb93c843c9a3d285b35734bd
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-08-30 10:38:31 +0200

    Implement a simple queue with unique values.

--- frontend-angular/app/scene-queue/scene-queue.component.js
+++ frontend-angular/app/scene-queue/scene-queue.component.js
@@ -1,20 +1,44 @@
 'use strict';
 
-angular.
-  module('sceneQueue').
-  component('sceneQueue', {
-    templateUrl: 'scene-queue/scene-queue.template.html',
-    controller: function SceneQueueController() {
-      this.scenes = [
-        {"id": 0, "title": "live piano stream"},
-        {"id": 1, "title": "please stand by"},
-        {"id": 2, "title": "youtube jingle"},
-        {"id": 3, "title": "twitch jingle"},
-        {"id": 4, "title": "facebook jingle"}
-      ];
-      this.selectedScene = 0;
-      this.selectScene = function(sceneId) {
-        this.selectedScene = sceneId;
-      };
-    }
-  });
+(function() {
+  // A simple queue with unique values.
+  function Queue() {
+    return {
+      "values": [],
+      "enqueue": function(value) {
+        if (this.values.indexOf(value) < 0) {
+          this.values.push(value);
+        }
+      },
+      "dequeue": function() {
+        if (this.values.length > 0) {
+          var value = this.values[0];
+          this.values = this.values.slice(1);
+          return value;
+        }
+      }
+    };
+  }
+  angular.
+    module('sceneQueue').
+    component('sceneQueue', {
+      templateUrl: 'scene-queue/scene-queue.template.html',
+      controller: function SceneQueueController() {
+        this.scenes = [
+          {"id": 0, "title": "live piano stream"},
+          {"id": 1, "title": "please stand by"},
+          {"id": 2, "title": "youtube jingle"},
+          {"id": 3, "title": "twitch jingle"},
+          {"id": 4, "title": "facebook jingle"}
+        ];
+        this.queue = Queue();
+        this.dequeued = undefined;
+        this.enqueue = function(value) {
+          this.queue.enqueue(value);
+        };
+        this.dequeue = function() {
+          this.dequeued = this.queue.dequeue();
+        };
+      }
+    });
+})();
